1e29461b178bc1129d56ac6a69dc582b31a93bfb
MIVAKI <elenaananenko2605@gmail.com>
2025-06-16 17:41:10 +0300

    Allure added

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 [![Java CI with Gradle](https://github.com/mivaki-not/patterns2.3z1/actions/workflows/gradle.yml/badge.svg)](https://github.com/mivaki-not/patterns2.3z1/actions/workflows/gradle.yml)
-![2025-06-16_17-23-28](https://github.com/user-attachments/assets/348bd24d-d451-4b39-9f35-10aad409d96d)
-![2025-06-16_17-24-02](https://github.com/user-attachments/assets/8cc471e6-54c1-416c-9bae-99d1601a30e2)
--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -1,7 +1,9 @@
 package ru.netology.delivery.test;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,9 +18,18 @@ import static com.codeborne.selenide.Selenide.open;
 
 class DeliveryTest {
 
+    @BeforeAll
+    static void setupAll(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
     @BeforeEach
-    void setup() {
-        open("http://localhost:9999");
+    public void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @BeforeEach
+    void setup() {open("http://localhost:9999");
     }
 
     @Test
